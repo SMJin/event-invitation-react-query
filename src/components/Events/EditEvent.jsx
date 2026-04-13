c7f8c2ed6a96b96bbc30db00c04ebe72436f1e7d
SMJin <sstaw@naver.com>
2026-04-13 14:40:30 +0900

    ★낙관적 업데이트★ - useMutation 에서 onMutate 함수 사용해서, 백엔드 통신 이전에  캐시로 선 업데이트

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -12,16 +12,29 @@ export default function EditEvent() {
   const navigate = useNavigate();
 
   const { data, isPending, isError, error } = useQuery({
-    queryKey: ['event', params.id],
+    queryKey: ['events', params.id],
     queryFn: ({signal}) => fetchEvent({ signal, id: params.id }),
   })
 
   const { mutate, isPending: isPendingUpdate, isError: isErrorUpdating, error: updateError } = useMutation({
     mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const newEvent = data.event; // 이미 캐싱되어 있는 데이터 (mutate 함수에 전달된 데이터)
+
+      // 낙관적 업데이트: 서버 응답을 기다리지 않고 바로 UI 업데이트
+      // ★ 주의할점. cancelQueries로 특정 키의 모든 활성 쿼리를 먼저 취소해야 함. ★
+      // 그렇지 않으면, 해당 키에 대한 활성 쿼리가 있다면, mutate 함수에 전달된 데이터로 업데이트하기 전에 서버에서 데이터를 가져오는 쿼리가 먼저 실행될 수 있음. (이 경우, 낙관적 업데이트가 무의미해짐)
+      // 이때, cancleQueries는 promise를 반환하므로 await로 처리해야 됨. 그럼 함수에도 async 키워드가 필요함.
+      await queryClient.cancelQueries({queryKey: ['events', params.id]});
+      queryClient.setQueryData(
+        ['events', params.id], // key
+        newEvent, // new data
+      );
+    }
   });
 
   function handleSubmit(formData) {
-    mutate({ id: params.id, event: formData });
+    mutate({ id: params.id, event: formData }); // 여기서 이미 react-query의 캐시가 업데이트됨
     navigate('../');
   }
 
