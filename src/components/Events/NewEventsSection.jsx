17ad93dbdac339c583a9c68734ab38a504aeb795
SMJin <sstaw@naver.com>
2026-04-13 09:33:03 +0900

    useQuery 로 events READ

--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -3,71 +3,32 @@ import { useEffect, useState } from 'react';
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 import EventItem from './EventItem.jsx';
+import { fetchEvents } from '../../util/http.js';
+import { useQuery } from '@tanstack/react-query';
 
 export default function NewEventsSection() {
-  const [data, setData] = useState();
-  const [error, setError] = useState();
-  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-      const response = await fetch('http://localhost:3000/events');
-
-      if (!response.ok) {
-        const error = new Error('An error occurred while fetching the events');
-        error.code = response.status;
-        error.info = await response.json();
-        throw error;
-      }
-
-      const { events } = await response.json();
-
-      return events;
-    }
-
-    fetchEvents()
-      .then((events) => {
-        setData(events);
-      })
-      .catch((error) => {
-        setError(error);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, []);
-
-  let content;
-
-  if (isLoading) {
-    content = <LoadingIndicator />;
-  }
-
-  if (error) {
-    content = (
-      <ErrorBlock title="An error occurred" message="Failed to fetch events" />
-    );
-  }
-
-  if (data) {
-    content = (
-      <ul className="events-list">
-        {data.map((event) => (
-          <li key={event.id}>
-            <EventItem event={event} />
-          </li>
-        ))}
-      </ul>
-    );
-  }
+  const {data, isLoading, isError, error} = useQuery({
+    queryKey: ['events'],
+    queryFn: fetchEvents,
+  });
 
   return (
     <section className="content-section" id="new-events-section">
       <header>
         <h2>Recently added events</h2>
       </header>
-      {content}
+      {isLoading && <LoadingIndicator />}
+      {isError && <ErrorBlock title="An error occurred" message={error.message} />}
+      {data && (
+        <ul className="events-list">
+          {data.map((event) => (
+            <li key={event.id}>
+              <EventItem event={event} />
+            </li>
+          ))}
+        </ul>
+      )}
     </section>
   );
 }
